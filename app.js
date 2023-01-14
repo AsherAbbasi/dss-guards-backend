dd901fb0ea38167d941e4fff5c16bbdb7fbe1674
Ibad Shah <iamibadd@gmail.com>
2023-01-15 03:55:19 +0500

    Subject: Added morgan for logging and cors as an env variable.

--- app.js
+++ app.js
@@ -6,44 +6,40 @@ const routes = require('./routes/v1')
 const ApiError = require('./utils/ApiErrors');
 const {jwtStrategy} = require('./config/passport');
 const passport = require('passport');
-const bodyParser=require('body-parser');
-
+const morgan = require('morgan');
 const app = express();
-
-
+const config = require('./config/config');
 
 // set security HTTP headers
 app.use(helmet());
 
+// logging
+app.use(morgan('tiny'))
+
 // parse json request body
 app.use(express.json());
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
 // parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 // enable cors
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
-app.use(cors(corsOptions))
+const corsOptions = {
+    origin: config.corsOrigin.split(' '),
+};
+app.use(cors(corsOptions));
 
 // jwt authentication
 app.use(passport.initialize());
 passport.use('jwt', jwtStrategy);
 
 // v1 api routes
-app.get('/', (req, res)=> res.send('Server is healthy!'));
+app.get('/', (req, res) => res.send('Server is healthy!'));
 
 app.use('/api', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+    next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
 
